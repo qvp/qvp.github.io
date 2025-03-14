9e77880697b9b0ea83fb0cc6dcb65baea25ae374
Alex K <bafojo@gmail.com>
2025-03-14 11:52:50 +0300

    remove gallery method

--- js/script.js
+++ js/script.js
@@ -3,33 +3,6 @@ new Vue({
     data: {
         data: DATA,
     },
-    methods: {
-        gallery(index) {
-            // $('#gallery' + index).show();
-
-            let images = [];
-            for (let i = 0; i < this.data[index]['gallery'].length; i++) {
-                images.push({src: this.data[index]['gallery'][i], srct: this.data[index]['gallery'][i], title: ''})
-            }
-
-            // $('#gallery' + index).on('galleryRenderEnd.nanogallery2', function(e) {
-            //     $('#gallery' + index).find('img').click();
-            // });
-
-            $('#gallery' + index).nanogallery2({
-                items: images,
-                thumbnailWidth: 'auto',
-                thumbnailHeight: 50,
-                thumbnailAlignment: 'center',
-                thumbnailLabel: {display: false},
-                thumbnailHoverEffect2: 'imageScaleIn80|labelAppear75',
-            });
-
-
-
-            // window.location.hash = '#nanogallery/gallery' + index + '/0/' + (index + 1);
-        },
-    },
     mounted() {
         for (let i = 0; i < this.data.length; i++) {
             if (this.data[i].gallery) {
